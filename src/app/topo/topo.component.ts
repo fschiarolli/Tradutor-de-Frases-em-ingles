21e2d4f4a17526be359113254dadca57400c28ee
Mac <mac@MacBook-Pro-de-Mac.local>
2018-01-02 21:38:47 -0200

    Criacao de novos componentes

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+    // tslint:disable-next-line:component-selector
     selector: 'app-topo',
-    templateUrl: './topo.compoent.html'
+    templateUrl: './topo.component.html',
+    styleUrls: ['./topo.component.css']
 })
 
 export class TopoComponent {
