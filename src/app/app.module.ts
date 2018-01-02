bd80b508c3725c409b2aa80951bbecca792a0253
Mac <mac@MacBook-Pro-de-Mac.local>
2018-01-02 20:57:24 -0200

    Iniciando a criacao do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TopoComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
